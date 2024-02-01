85a4f8c15a5ac3c9b5f766a26dbaa7255dbc2c63
Martisius <S02835598@ccc.ccofc.edu>
2024-02-01 10:08:52 -0700

    firstHelloWorld

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,24 @@
+package helloWorld
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello Cruel");
+		System.out.print("World");
+
+	}
+
+}
+
+
+
+
+public class Hello3 {
+	
+	public static void main9Strong[] args {
+		System.out.print("Hello!\nHow are you doing?\n);"
+	}
+}
