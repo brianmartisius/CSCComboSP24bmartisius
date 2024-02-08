c43e72eb82619383474a6c2154cf6d5adcf3704a
Martisius <S02835598@ccc.ccofc.edu>
2024-02-08 10:13:14 -0700

    02/08/24

---
+++ Chapter 5/src/WeekDays.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class WeekDays {
+	static Scanner input = new Scanner (System.in);
+	public static void main(String[] args) {
+		int dayNum = 0;
+		String dayName;
+		while(dayNum < 1 || dayNum > 7) {
+			System.out.println("Enter number " + "of the day of the week (1-7):");
+			dayNum = input.nextInt();
+		}
+		
+		
+		dayName = getWeekDay(dayNum);
+		System.out.println("The day of the week is " + dayName);
+		
+
+	}
+	private static String getWeekDay(int dayNum) {
+		String day;
+		switch (dayNum) {
+		case 1:
+			day = "Sunday";
+			break;
+		case 2:
+			day = "Monday";
+			break;
+		case 3:
+			day = "Tuesday";
+			break;
+		case 4:
+			day = "Wednesday";
+			break;
+		case 5:
+			day = "Thursday";
+			break;
+		case 6:
+			day = "Friday";
+			break;
+		case 7:
+			day = "Saturday";
+			break;
+		default:
+			day = "Unknown";
+			break;
+		}
+		return day;
+	}
+
+}
